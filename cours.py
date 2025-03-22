461a74b977da7f84048be1626ae59e72e401c306
mickaelgall <mickaelgall6@gmail.com>
2025-03-22 12:09:59 +0100

    0.1

---
+++ cours.py
@@ -0,0 +1,23 @@
+
+def main():
+   # création d'une variable 'mon-prénom' ayant pour valeur le mot Mickaël
+    mon_prénom = "Mickaël"
+    mon_prénom = "youtube"
+
+   # création d'une variable 'age' ayant pour valeur 21
+    age = 21
+    # affiche le mon_prénom et l'age
+    #change la valeur de age avec 22
+    age = 22
+    #affiche la nouvelle valeur
+    age = (age) * age
+    print("salutations " + mon_prénom + ", j'ai " + str(age) + " ans !")
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
